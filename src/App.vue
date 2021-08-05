30be443e6d506db2d14f70c6f118c432c292106b
wndud0159 <wndud0159@naver.com>
2021-08-05 10:56:59 +0900

    main page worked

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
               <img 
               src="/profile.jpeg" 
               class="w-10 y-10 rounded-full">
-              <i class="fas fa-sort-down text-xl"></i>
+              <i class="fas fa-sort-down text-xl text-gray-400"></i>
             </div>
           </div>
           <div class="absolute right-0 mt-px top-6 text-gray-500 hover:text-primary cursor-pointer hidden pb-2" >
@@ -25,8 +25,8 @@
       <div class="flex items-center h-20 -mb-1">
         <!-- title  logo-->
         <div class=" font-extrabold text-2xl flex-1 text-primary flex items-center">
-          <i class="fab fa-node-js text-4xl"></i>
-          <span>12jobs</span>
+          <i class="fas fa-feather-alt text-4xl mr-1"></i>
+          <span>iback</span>
         </div>
         <!-- 메뉴 -->
         <div class=" hidden lg:inline-block w-4/5 text-gray-500 ">
@@ -35,13 +35,13 @@
             <span>홈</span>
           </div>
           <div class=" flex items-center justify-center h-20 pt-2   w-1/6 cursor-pointer border-b-4 border-gray-100 hover:border-primary hover:text-primary">
-            <span>구직자</span>
+            <span>아이백이란?</span>
           </div>
           <div class=" flex items-center justify-center h-20 pt-2   w-1/6 cursor-pointer border-b-4 border-gray-100 hover:border-primary hover:text-primary">
-            <span>구인자</span>
+            <span>사업제휴</span>
           </div>
           <div class=" flex items-center justify-center h-20 pt-2   w-1/6 cursor-pointer border-b-4 border-gray-100 hover:border-primary hover:text-primary">
-            <span>협력업체신청</span>
+            <span>고객센터</span>
           </div>
           </div>
         </div>
@@ -50,36 +50,52 @@
 
     
 
+    <!-- main section -->
+      <div>
+        <img id="image-height" class="mt-20 h-auto w-full object-cover"
+        src="http://picsum.photos/id/119/2000/1000">
+      </div>
 
+    <!-- main text -->
+    <div class="fixed  lg:bottom-48 sm:bottom-60 bottom-32 flex flex-col items-center w-full">
+      <div class="mb-10 lg:text-4xl sm:text-3xl text-2xl">
+        <div class="">죽음을 잘 준비하고 싶으신가요?</div>
+        <div class="">아이백이 가이드가 되어 드릴게요.</div>
+      </div>
+      <div class=" lg:text-2xl sm:text-xl text-xs mb-10">매년 120만 명의 사람들이 죽음과 슬픔을 치유하기 위해 우리와 함께해요</div>
 
+      <button class="text-white text-xl border border-primary bg-primary py-2 px-3 rounded-xl shadow-2xl hover:bg-dark"
+      >무료 계획 세워보기!</button>
+    </div>
 
+    <!-- main button -->
    
 
     <!--bottom mobile menu -->
-    <div class=" fixed border-t-4 border-gray-100 bottom-0 w-full bg-white text-gray-500 z-40">
+    <div class="  fixed border-t-4 lg:border-0 border-gray-100 bottom-0 w-full bg-white text-gray-500 z-40">
       <div class="flex items-center justify-center text-center text-sm lg:hidden py-3">
         <div class=" w-1/4 py-2 flex flex-col space-y-1 cursor-pointer hover:text-primary">
-          <i class="fas fa-home"></i>
+          <i class="fas fa-home "></i>
           <span>홈</span>
         </div>
         <div class=" w-1/4 py-2 flex flex-col space-y-1 cursor-pointer hover:text-primary">
-          <i class="fas fa-hard-hat"></i>
-          <span>구인자</span>
+          <i class="fas fa-book "></i>
+          <span>아이백이란?</span>
         </div>
         <div class=" w-1/4 py-2 flex flex-col space-y-1 cursor-pointer hover:text-primary">
-          <i class="fas fa-truck-pickup"></i>
-          <span>구직자</span>
+          <i class="fas fa-envelope "></i>
+          <span>사업제휴</span>
         </div>
         <div class=" w-1/4 py-2 flex flex-col space-y-1 cursor-pointer hover:text-primary">
-          <i class="fas fa-address-card"></i>
-          <span>협력업체신청</span>
+          <i class="fas fa-chalkboard "></i>
+          <span>고객센터</span>
         </div>
       </div>
     </div>
 
     <!-- side toggle menu -->
     <div :class="`${sideToggleMenu ? 'md:w-1/5 w-3/5' : 'w-0'} fixed right-0 bg-white  h-full  z-50 transition-all`">
-    <i @click="sideToggleMenu = false" class="fas fa-times px-4 py-3 hover:bg-gray-100 rounded-full"></i>
+    <i @click="sideToggleMenu = false" class="fas fa-times px-4 py-3 hover:bg-gray-100 rounded-full text-gray-400"></i>
     </div>
   </div>  
  
@@ -112,5 +128,7 @@ setup() {
 </script>
 
 <style>
-
+#image-height {
+  /* height: 1000px */
+}
 </style>
