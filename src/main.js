3dafa622fc30d9e4de8aa57537910e029a2a77f0
최주영 <73331008+wndud0159@users.noreply.github.com>
2022-02-11 12:46:28 +0900

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,6 +5,6 @@ import './index.css'
 import store from './store'
 import VueDaumPostcode from 'vue-daum-postcode'
 
-window.Kakao.init('27c3bac20c362a640406590367df687d')
+
 
 createApp(App).use(router).use(store).use(VueDaumPostcode).mount('#app')
